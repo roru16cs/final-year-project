589be2a864f6bdfcb6269dbe7af735a508e45ce5
roru16cs <roru16cs@cmrit.ac.in>
2020-03-29 18:20:23 +0530

    Changed and integrated registration template

--- assyst/accounts/views.py
+++ assyst/accounts/views.py
@@ -27,27 +27,20 @@ def logout(request):
 def register(request):
 
 	if request.method == 'POST':
-		first_name = request.POST['first_name']
-		last_name = request.POST['last_name']
+		name = request.POST['name']
 		username = request.POST['username']
 		email = request.POST['email']
-		if request.POST['password1'] == request.POST['password2']:
-			if User.objects.filter(username = username).exists():
-				messages.info(request,'Username already taken')
-				return redirect('register')
-			elif User.objects.filter(email = email).exists():
-				messages.info(request, 'Email already taken')
-				return redirect('register')
-			password = request.POST['password1']
-			user = User.objects.create_user(username =  username, password = password, email = email,
-		 		first_name = first_name, last_name = last_name)
-			user.save()
-			print('User created')
-			return redirect('login')
-		else:
-			print('Passwords dont match')
+		if User.objects.filter(username = username).exists():
+			messages.info(request,'Username already taken')
 			return redirect('register')
-		return redirect('/')
-	
+		elif User.objects.filter(email = email).exists():
+			messages.info(request, 'Email already taken')
+			return redirect('register')
+		password = request.POST['password']
+		user = User.objects.create_user(username =  username, password = password, email = email,
+	 		first_name = name)
+		user.save()
+		print('User created')
+		return redirect('login')
 	else:
 		return render(request,'register.html')
--- assyst/assyst/settings.py
+++ assyst/assyst/settings.py
@@ -119,4 +119,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [ os.path.join(BASE_DIR,'assyst/static')]
-STATIC_ROOT = os.path.join(BASE_DIR,'assests')
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
